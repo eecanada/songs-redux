964d5988675bd5e8a71cad7e7061fcf6d41adec1
Eder Canada <canada.eder@gmail.com>
2021-10-22 10:33:02 -0700

    connect

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import SongList from './SongList';
 
-const App = () => {
-  return <div>Hi</div>;
+const App = () => { 
+  return (
+    <div> 
+      <SongList/>
+    </div>
+  )
 };
 
 export default App;
