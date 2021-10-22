3e9e7e5c4dec63f1cdb29b7c828700c23855b630
Eder Canada <canada.eder@gmail.com>
2021-10-22 10:09:04 -0700

    redux

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+  return [
+    { title: 'Can I Live', duration: '3:21' },
+    { title: 'Sigo Extrañándote', duration: '4:21' },
+    { title: 'Happier', duration: '3:39' },
+    { title: 'm.A.A.d city', duration: '5:40' },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if ((action.type = 'SONG_SELECTED')) {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});
