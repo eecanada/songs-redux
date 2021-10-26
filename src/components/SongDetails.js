8d14b4333879ba5ba6881aedee2437469ce86823
Eder Canada <canada.eder@gmail.com>
2021-10-26 16:55:11 -0700

    props

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react';
-
+import React from 'react';
+import { connect } from 'react-redux';
 
 const SongDetails = () => {
-  return (  <div> Song Details </div>);
-}
- 
-export default SongDetails;
+  return <div> Song Details </div>;
+};
+
+const mapStateToProps = (state) => {
+  {
+    song: state.selectedSong;
+  }
+};
+
+export default connect(mapStateToProps)(SongDetails);
